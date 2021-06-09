be418ca64b1e02bc3793ddb42369f3089cd2208b
i389161 <i389161@fhict.nl>
2021-06-09 19:31:17 +0200

    Add profile page

--- kwetter-frontend/src/app/components/profile/profile.component.ts
+++ kwetter-frontend/src/app/components/profile/profile.component.ts
@@ -11,6 +11,8 @@ import { ProfileService } from 'src/app/services/profile/profile.service';
 })
 export class ProfileComponent implements OnInit {
     public profile?: Profile;
+    public following?: Profile[];
+    public followers?: Profile[];
 
     constructor(private route: ActivatedRoute, private profileService: ProfileService, private router: Router) {}
 
@@ -19,6 +21,17 @@ export class ProfileComponent implements OnInit {
             this.profileService.getProfileById(params['id']).subscribe(resp => {
                 if(resp.ok && resp.body) {
                     this.profile = resp.body;
+                    this.profileService.getFollowed(this.profile.id).subscribe(followedResp => {
+                        if(followedResp.ok && followedResp.body) {
+                            this.following = followedResp.body;
+                        }
+                    });
+
+                    this.profileService.getFollowers(this.profile.id).subscribe(followingResp => {
+                        if(followingResp.ok && followingResp.body) {
+                            this.followers = followingResp.body;
+                        }
+                    });
                 }
             }, 
             error => {
